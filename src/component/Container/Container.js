e1f0998e29f7e0405258f0850219f52e3dbad530
eslamOsamaDeveloper <eslamosama.work100@gmail.com>
2024-05-06 15:17:39 +0200

    upgrade react to 18.3.1

--- src/component/Container/Container.js
+++ src/component/Container/Container.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Container.css"
+import "./Container.css";
 import Timeing from "../Timeing/Timeing";
 import TimeTable from "../TimeTable/TimeTable";
 import TaskItem from "../TaskItem/TaskItem";
@@ -7,59 +7,78 @@ import DateComponent from "../DateComponent/DateComponent";
 import Status from "../Status/Status";
 import CreateTask from "../CreateTask/CreateTask";
 function Container() {
-  const [taskItem, settaskItem] = useState([])
-  const [allTasks, setallTasks] = useState([])
-  const [tasksByDay, settasksByDay] = useState([])
+  const [taskItem, settaskItem] = useState([]);
+  const [allTasks, setallTasks] = useState([]);
+  const [tasksByDay, settasksByDay] = useState([]);
   useEffect(() => {
-    let tasks = [...allTasks, taskItem]
-    setallTasks(tasks)
-  }, [taskItem])
+    let tasks = [...allTasks, taskItem];
+    setallTasks(tasks);
+  }, [taskItem]);
   useEffect(() => {
-    showTasksByDate()
-  }, [allTasks])
+    showTasksByDate();
+  }, [allTasks]);
   useEffect(() => {
-    let filterItAll = allTasks
-    filterItAll.filter(i=>{return i.length !== 0})
-    setallTasks(filterItAll)
-  }, [])
-  let day = ()=>{
-    let year = new Date().getFullYear()
-    let mounth = new Date().getMonth()
-    let day = new Date().getDate()
-    return `${year} / ${mounth + 1} / ${day}`
-  }
-  let showTasksByDate = ()=>{
-    let year = new Date().getFullYear()
-    let mounth = new Date().getMonth() + 1
-    let day = new Date().getDate()
+    let filterItAll = allTasks;
+    filterItAll.filter((i) => {
+      return i.length !== 0;
+    });
+    setallTasks(filterItAll);
+  }, []);
+  let day = () => {
+    let year = new Date().getFullYear();
+    let mounth = new Date().getMonth();
+    let day = new Date().getDate();
+    return `${year} / ${mounth + 1} / ${day}`;
+  };
+  let showTasksByDate = () => {
+    let year = new Date().getFullYear();
+    let mounth = new Date().getMonth() + 1;
+    let day = new Date().getDate();
     if (day < 10) {
-      let thisDay = `${year}-${mounth}-0${day}`
-      let theFilter = allTasks.filter(x=>{return x[1] == thisDay})
-      settasksByDay(theFilter)
+      let thisDay = `${year}-${mounth}-0${day}`;
+      let theFilter = allTasks.filter((x) => {
+        return x[1] === thisDay;
+      });
+      settasksByDay(theFilter);
     } else {
-      let thisDay = `${year}-${mounth}-${day}`
-      let theFilter = allTasks.filter(x=>{return x[1] == thisDay})
-      settasksByDay(theFilter)
+      let thisDay = `${year}-${mounth}-${day}`;
+      let theFilter = allTasks.filter((x) => {
+        return x[1] === thisDay;
+      });
+      settasksByDay(theFilter);
     }
-  }
+  };
   return (
     <div id="container">
-      <h2> <DateComponent day={day}/>  <Status tasksByDay={tasksByDay}/></h2>
+      <h2>
+        {" "}
+        <DateComponent day={day} /> <Status tasksByDay={tasksByDay} />
+      </h2>
       <div className="data">
         <div className="data-real">
           <ul>
-          {tasksByDay ? tasksByDay.map((i,y)=>{
-            return  <TaskItem key={y} taskName={i[0]} taskTime={i[2]} setallTasks={setallTasks} allTasks={allTasks}/>
-          }) : "no Tasks"}
+            {tasksByDay
+              ? tasksByDay.map((i, y) => {
+                  return (
+                    <TaskItem
+                      key={y}
+                      taskName={i[0]}
+                      taskTime={i[2]}
+                      setallTasks={setallTasks}
+                      allTasks={allTasks}
+                    />
+                  );
+                })
+              : "no Tasks"}
           </ul>
         </div>
         <div className="data-detailes">
-          <TimeTable tasksByDay={tasksByDay}/>
-          <Timeing/>
+          <TimeTable tasksByDay={tasksByDay} />
+          <Timeing />
         </div>
       </div>
-      <CreateTask settaskItem={settaskItem}/>
+      <CreateTask settaskItem={settaskItem} />
     </div>
-  )
+  );
 }
-export default Container
+export default Container;
--- src/component/TimeTable/TimeTable.js
+++ src/component/TimeTable/TimeTable.js
@@ -1,36 +1,49 @@
-import React , {useEffect} from 'react'
-import "./TimeTable.css"
-function TimeTable({tasksByDay}) {
-    useEffect(() => {
-        drawTime()
-    }, [tasksByDay])
-    
-    let dayArr = []
-    let dayTime = ()=>{
+import React, { useEffect } from "react";
+import "./TimeTable.css";
+function TimeTable({ tasksByDay }) {
+  useEffect(() => {
+    drawTime();
+  }, [tasksByDay]);
+
+  let dayArr = [];
+  let dayTime = () => {
     let days = 24;
-        for (let i = 1; i <= days; i++) {
-        dayArr.push(i)
-        }
+    for (let i = 1; i <= days; i++) {
+      dayArr.push(i);
     }
-    const makeTimeGlow = time=>{
-        let taskDone = tasksByDay.filter(i=>{
-            return i[2] == time || i[2].slice(0,2)== time.slice(0,2)
-        })
-        if (taskDone.length != 0) {
-            return true
-        }
+  };
+  const makeTimeGlow = (time) => {
+    let taskDone = tasksByDay.filter((i) => {
+      return i[2] === time || i[2].slice(0, 2) === time.slice(0, 2);
+    });
+    if (taskDone.length !== 0) {
+      return true;
     }
-    const drawTime=()=>{
-        return dayArr.map(i=>{
-            return  i < 10 ? <li key={i} className={`time-table ${makeTimeGlow("0"+i+":00") ? "glow" : ""}`}> 0{i}:00</li> 
-            : <li key={i} className={`time-table ${makeTimeGlow(i+":00") ? "glow" : ""}`}> {i}:00</li>
-        })
-    }
-    dayTime()
-    return (
-        <ul>
-            {drawTime()}
-        </ul>
-    )
+  };
+  const drawTime = () => {
+    return dayArr.map((i) => {
+      return i < 10 ? (
+        <li
+          key={i}
+          className={`time-table ${
+            makeTimeGlow("0" + i + ":00") ? "glow" : ""
+          }`}
+        >
+          {" "}
+          0{i}:00
+        </li>
+      ) : (
+        <li
+          key={i}
+          className={`time-table ${makeTimeGlow(i + ":00") ? "glow" : ""}`}
+        >
+          {" "}
+          {i}:00
+        </li>
+      );
+    });
+  };
+  dayTime();
+  return <ul>{drawTime()}</ul>;
 }
-export default TimeTable
+export default TimeTable;
